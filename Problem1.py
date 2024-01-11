4dec38fe2bd791b79a82d6a7f5fcabbce6da11bc
Abdullah Al-Munaikh <Amunaikh@gmail.com>
2024-01-11 10:49:54 +0000

    Refactor birthday generation and duplicate checking

--- Problem1.py
+++ Problem1.py
@@ -1,60 +1,67 @@
 import random
 import convert
 
-names_list = [
-    "John",
-    "Mary",
-    "Bob",
-    "Martha",
-    "Linda",
-    "James",
-    "Elizabeth",
-    "Michael",
-    "Patricia",
-    "William",
-    "Jennifer",
-    "David",
-    "Barbara",
-    "Richard",
-    "Susan",
-    "Joseph",
-    "Jessica",
-    "Thomas",
-    "Sarah",
-    "Charles",
-    "Karen",
-    "Christopher",
-    "Nancy",
-    "Daniel",
-    "Lisa",
-    "Matthew",
-    "Betty",
-    "Anthony",
-    "Margaret",
-    "Donald",
-    "Sandra",
-    "Mark",
-    "Ashley",
-    "Paul",
-    "Dorothy",
-]
 
-
+# Temporary storage for birthday data for each trial
 temp_people_data = {}
+
+# List of dictionaries containing the birthday data
 people_data = []
 
 
-# This function generates random birthdays for a given number of people
+# Generate random birthdays for a specified number of people
 def generate_birthdays(num_of_people):
     possible_birthdays = 365
     random_birthdays = []
     random_names = []
+    # List of random names for the random people
+    names_list = [
+        "John",
+        "Mary",
+        "Bob",
+        "Martha",
+        "Linda",
+        "James",
+        "Elizabeth",
+        "Michael",
+        "Patricia",
+        "William",
+        "Jennifer",
+        "David",
+        "Barbara",
+        "Richard",
+        "Susan",
+        "Joseph",
+        "Jessica",
+        "Thomas",
+        "Sarah",
+        "Charles",
+        "Karen",
+        "Christopher",
+        "Nancy",
+        "Daniel",
+        "Lisa",
+        "Matthew",
+        "Betty",
+        "Anthony",
+        "Margaret",
+        "Donald",
+        "Sandra",
+        "Mark",
+        "Ashley",
+        "Paul",
+        "Dorothy",
+    ]
 
+    
+    # Loops through the specified number of times
     for i in range(num_of_people):
+        # Generating a random birthday for a randomly chosen name and storing them in a temporary dictionary.
         random_bd = random.randrange(1, possible_birthdays + 1)
         random_name = names_list[random.randrange(0, len(names_list))]
         random_names.append(random_name)
         random_birthdays.append(random_bd)
+        # If a name already exists, it adds a suffix to avoid duplicate names.
         if random_name not in temp_people_data.keys():
             temp_people_data[random_name] = random_bd
         else:
@@ -62,40 +69,49 @@ def generate_birthdays(num_of_people):
     return random_birthdays
 
 
-# This function checks if there are duplicate birthdays in a list
+# Check if any birthdays in a list are duplicates
 def has_duplicate(birthdays):
+    # Creating an empty set
     seen_birthdays = set()
 
+    # Iterate through the list of generated birthdays.
     for birthday in birthdays:
+        # If a birthday already exists in the set, it means it's a duplicate.
         if birthday in seen_birthdays:
             # find the name of the preson with the same birthday
             return True
+        # Add each birthday to the set.
         seen_birthdays.add(birthday)
 
     return False
 
 
-# This function calculates the number of duplicate birthdays in a given number of trials
+# Calculate the number of trials with duplicate birthdays
 def num_of_duplicates(trials, num_of_people):
     num_of_duplicates = 0
 
+    # Loops through the specified number of trials
     for _ in range(trials):
+        # Clearing the temporary data at the start of every trial
         temp_people_data.clear()
+        # Generating new birthdays on each run.
         generated_birthdays = generate_birthdays(num_of_people)
 
+        # Checks for duplicates and increases the counter if found.
         if has_duplicate(generated_birthdays):
             num_of_duplicates += 1
+        # Copying the temporary data to the permanent people data
         people_data.append(temp_people_data.copy())
     return num_of_duplicates
 
 
-# This function calculates the percentage of trials that resulted in duplicate birthdays
+# Calculate the percentage of trials with duplicates
 def persentage(trials, people_num=23):
-    duplicate_rate = (num_of_duplicates(trials, people_num) / trials) * 100
-    return duplicate_rate
+    persentage = (num_of_duplicates(trials, people_num) / trials) * 100
+    return persentage
 
 
-# This function calculates the average percentage of trials that resulted in duplicate birthdays over 10 runs
+# Calculate the average percentage of duplicates over multiple runs
 def average(trials, people_num=23):
     total_rate = 0
 
@@ -105,14 +121,23 @@ def average(trials, people_num=23):
     return total_rate / 10
 
 
+# Ask for number of simulation runs
 trials = int(input("Enter the number of trials: "))
+# Ask for number of people per run
 people_num = int(input("Enter the number of people: "))
 
 
+# Calculate percentage of duplicate birthdays
 duplicate_probability = persentage(trials, people_num)
-print(convert.change_dict(people_data))
+
+
+# Show the total number of trials simulated
 print(len(people_data))
+
+# Show the results
 print(
     f"The chance of similar birthdays among 23 people is "
     f"{duplicate_probability:.2f}%"
 )
+# print(convert.change_dict(people_data))
+
